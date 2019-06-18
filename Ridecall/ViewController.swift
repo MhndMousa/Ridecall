a6187cfe120b1456e9e0ddeccdf2d3eae1c62f67
Muhannad Alnemer <mhndmsa@gmail.com>
2019-06-17 18:08:19 -0700

    Initial Commit

---
+++ Ridecall/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ridecall
+//
+//  Created by Muhannad Alnemer on 6/17/19.
+//  Copyright © 2019 Muhannad Alnemer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
